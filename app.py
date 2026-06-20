24bd351965084d32faf992148a95a126dfd56941
Aryaman <aryaman.agarwal.ug23@nsut.ac.in>
2026-06-21 00:37:23 +0530

    restructure: move frontend into frontend/ dir, add backend unit tests

--- app.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from backend.app import app
-
-
-if __name__ == "__main__":
-    host = os.getenv("FLASK_HOST", "127.0.0.1")
-    port = int(os.getenv("PORT", "5000"))
-    app.run(host=host, port=port, debug=True)
